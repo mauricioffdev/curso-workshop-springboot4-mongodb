38c867f3b5e6745a86c177d04b9a813a9a62782f
mauricioffdev <seu-email@exemplo.com>
2026-03-11 18:34:05 -0300

    Obtendo um usuário por id

--- src/main/java/com/mauricioff/workshopmongo/services/UserService.java
+++ src/main/java/com/mauricioff/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.mauricioff.workshopmongo.services;
 
 import com.mauricioff.workshopmongo.domain.User;
 import com.mauricioff.workshopmongo.repository.UserRepository;
+import com.mauricioff.workshopmongo.services.services.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -13,7 +15,13 @@ public class UserService {
     @Autowired
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
---
+++ src/main/java/com/mauricioff/workshopmongo/services/services/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.mauricioff.workshopmongo.services.services;
+
+public class ObjectNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
