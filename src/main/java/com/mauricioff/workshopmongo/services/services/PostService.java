dab0135e69e70afe029f8a5189a3125121ce971f
mauricioffdev <seu-email@exemplo.com>
2026-03-13 20:39:35 -0300

    Obtendo um post por id

---
+++ src/main/java/com/mauricioff/workshopmongo/services/services/PostService.java
@@ -0,0 +1,20 @@
+package com.mauricioff.workshopmongo.services.services;
+
+import com.mauricioff.workshopmongo.domain.Post;
+import com.mauricioff.workshopmongo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
