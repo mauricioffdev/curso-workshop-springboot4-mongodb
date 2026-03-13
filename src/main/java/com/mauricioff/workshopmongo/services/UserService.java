0ac44b8ae3a4170efc15054bb3f39e0e5684a6a4
mauricioffdev <seu-email@exemplo.com>
2026-03-12 22:00:21 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/mauricioff/workshopmongo/services/UserService.java
+++ src/main/java/com/mauricioff/workshopmongo/services/UserService.java
@@ -25,11 +25,16 @@ public class UserService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return repo.insert(obj);
     }
 
-    public User fromDTO(UserDTO objDto){
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
